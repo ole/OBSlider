8346f44b532b059373b0df96861f5b94f42152bf
Ole Begemann <ole@oleb.net>
2011-01-03 14:31:11 +0100

    Add demo project (Xcode View-based application)

---
+++ OBSliderDemo/Classes/OBSliderDemoAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  OBSliderDemoAppDelegate.h
+//  OBSliderDemo
+//
+//  Created by Ole Begemann on 03.01.11.
+//  Copyright 2011 Ole Begemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class OBSliderDemoViewController;
+
+@interface OBSliderDemoAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+    OBSliderDemoViewController *viewController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet OBSliderDemoViewController *viewController;
+
+@end
+
---
+++ OBSliderDemo/Classes/OBSliderDemoViewController.h
@@ -0,0 +1,16 @@
+//
+//  OBSliderDemoViewController.h
+//  OBSliderDemo
+//
+//  Created by Ole Begemann on 03.01.11.
+//  Copyright 2011 Ole Begemann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OBSliderDemoViewController : UIViewController {
+
+}
+
+@end
+
